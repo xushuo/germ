008943424b74d96e2e0f84b733ad567485f5914a
xushuo <xushuo@fclassroom.com>
2017-11-08 10:37:41 +0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,62 @@
+/*$('#fullpageContent').fullpage({
+ controlArrows:false,
+ sectionSelector:'.sectionF',
+ slideSelector:'.slideF',
+ afterRender:function(){
+ console.log('init');
+ },
+ afterSlideLoad: function(anchorLink,index,slideAnchor,slideIndex){
+ }
+ });*/
+
+var myChart = echarts.init(document.getElementById('chart'));
+var option = {
+    title: {
+        text: '细菌分裂',
+        textStyle:{
+            fontSize: 40
+        }
+    },
+    tooltip: {},
+    animationDurationUpdate: 1500,
+    animationEasingUpdate: 'quinticInOut',
+    series: {
+        type: 'graph',
+        layout: 'force',
+        symbol: 'image://img/germ.png',
+        symbolSize: 80,
+        roam: true,
+        draggable: true,
+        label: {
+            normal: {
+                show: true
+            }
+        },
+        force: {
+            repulsion: 200
+        },
+        data: [{
+            id: 0,
+            draggable: true
+        }],
+        links: []
+    }
+};
+var num = 2;
+myChart.on('dblclick', function (params) {
+    console.log(option);
+    var strs = [];
+    for(var i=0; i<num; i++){
+        strs.push({
+            id: i
+        });
+    }
+    option.series.data = strs;
+    num = num*2;
+    myChart.setOption(option);
+});
+myChart.hideLoading();
+myChart.setOption(option);
+window.onresize = function () {
+    myChart.resize();
+}
